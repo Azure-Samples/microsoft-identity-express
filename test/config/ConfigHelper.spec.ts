ccab360f113e3cfa8a8febfd32ed39a51b5c93d8
Dogan Erisen <dogan.erisen@gmail.com>
2022-06-04 17:52:38 -0700

    fix tests

--- test/config/ConfigHelper.spec.ts
+++ test/config/ConfigHelper.spec.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { AppSettings } from '../../src/config/AppSettings';
+import { WebAppSettings } from '../../src/config/AppSettings';
 import { ConfigHelper } from '../../src/config/ConfigHelper';
 import { TEST_APP_SETTINGS } from '../TestConstants';
 
@@ -24,14 +24,14 @@ describe('Configuration helper tests', () => {
     });
 
     it('should get resource name from a given list of scopes', () => {
-        const appSettings: AppSettings = TEST_APP_SETTINGS;
+        const appSettings: WebAppSettings = TEST_APP_SETTINGS;
         const scopes = TEST_APP_SETTINGS.protectedResources.armAPI.scopes;
         const resourceName = ConfigHelper.getResourceNameFromScopes(scopes, appSettings);
         expect(resourceName).toEqual(Object.keys(TEST_APP_SETTINGS.protectedResources)[1]);
     });
 
     it('should get scopes from a given endpoint in the settings file', () => {
-        const appSettings: AppSettings = TEST_APP_SETTINGS;
+        const appSettings: WebAppSettings = TEST_APP_SETTINGS;
         const endpoint = TEST_APP_SETTINGS.protectedResources.armAPI.endpoint;
         const scopes = ConfigHelper.getScopesFromResourceEndpoint(endpoint, appSettings);
         expect(scopes).toEqual(TEST_APP_SETTINGS.protectedResources.armAPI.scopes);
